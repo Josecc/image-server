0347c3d271bf70ff061c5b3781a15b1578b1010c
Jose Canahui <jose.canahui@gmail.com>
2016-07-03 21:24:53 -0400

    resolved check for files if not answer with not found

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ let images = __dirname;
 
 app.use('/images/:im_name', (req, res, next) => {
   let extensions = ['.png', '.jpg'];
+  var exists = false;
+  let runs = 1;
   extensions.forEach((extension) => {
     fs.exists( images + '/images/' + req.params.im_name + extension, (exists) => {
       if(exists) {
         req.url += extension;
         next();
+      } else if (runs == extensions.length){
+        next();
       }
+      runs++;
     });
   });
-  next();
 });
 
 app.use(express.static(images));
